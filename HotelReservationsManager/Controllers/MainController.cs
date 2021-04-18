6b027671f513b8a72035ca9fe2232ad1272fbf2c
tswetti <tswetti37@gmail.com>
2021-04-18 10:44:16 +0300

    Minor adjustements

--- HotelReservationsManager/Controllers/MainController.cs
+++ HotelReservationsManager/Controllers/MainController.cs
@@ -14,15 +14,12 @@ namespace HotelReservationsManager.Controllers
             byte[] bufferUser = new byte[200];
             byte[] bufferFName = new byte[200];
             byte[] bufferLName = new byte[200];
-            //byte[] bufferAdmin = new byte[200];
             if (HttpContext.Session.TryGetValue("username", out bufferUser))
             {
                 HttpContext.Session.TryGetValue("firstname", out bufferFName);
                 HttpContext.Session.TryGetValue("lastname", out bufferLName);
-                //HttpContext.Session.TryGetValue("admin", out bufferAdmin);
                 ViewData["names"] = Encoding.UTF8.GetString(bufferFName) + " " +
                                     Encoding.UTF8.GetString(bufferLName);
-                //bool admin = BitConverter.ToBoolean(bufferAdmin);
                 return View();
             }
             return RedirectToAction("Login", "Users");
